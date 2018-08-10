6e680e2a7680c9ceea41a9681a7fe292705b3cb2
Jefford Ray Mamacus <jefford@ingenuity.ph>
2018-08-10 12:18:08 +0800

    Added fields on model + collected static files

--- content/models.py
+++ content/models.py
@@ -6,6 +6,7 @@ class Header(models.Model):
     title = models.CharField(max_length=100)
     subtitle = models.CharField(max_length=200)
     image = models.ImageField(upload_to = 'header/')
+    video = models.URLField(max_length=200, default="")
 
 
 class Section(models.Model):
@@ -13,3 +14,15 @@ class Section(models.Model):
     content = HTMLField()
     section_image = models.ImageField(upload_to = 'section/')
     subquote_image = models.ImageField(upload_to = 'section/')
+
+class Fact(models.Model):
+    title = models.CharField(max_length=20)
+    value = models.PositiveIntegerField()
+
+class SocialMedia(models.Model):
+    facebook = models.URLField(max_length=200, default="")
+    twitter = models.URLField(max_length=200, default="")
+    instagram = models.URLField(max_length=200, default="")
+    linkedin = models.URLField(max_length=200, default="")
+    telephone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100, default="")
