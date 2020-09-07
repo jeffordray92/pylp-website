a107b2c478eeb354c8885ebc3878f9161255e2f7
Zaldy Pagaduan <zaldypagaduanjr@gmail.com>
2020-09-07 11:44:10 +0800

    Added custom upload adapter for ckeditor

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
-from news.views import NewsListView, NewsView, SubmitArticleView
+from django.conf.urls import url
+from news.views import NewsListView, NewsView, SubmitArticleView, simple_upload
 
 urlpatterns = [
     path('', NewsListView.as_view(), name='news_list'),
     path('<str:tag>', NewsListView.as_view(), name='news_list'),
     path('article/<slug:slug>/', NewsView.as_view(), name='news'),
-    path('submit-new/', SubmitArticleView.as_view(), name='submit-news')
+    path('submit-new/', SubmitArticleView.as_view(), name='submit-news'),
+    url(r'^upload-image/', simple_upload, name='upload-image'),
 ]
